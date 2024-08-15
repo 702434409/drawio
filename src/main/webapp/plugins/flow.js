b78b60f533ad2054f11a43b97471528b5ef32dae
702434409 <127304242+702434409@users.noreply.github.com>
2024-08-15 19:36:37 +0800

    Update flow.js
    修改flow.js支持记忆动态线

--- src/main/webapp/plugins/flow.js
+++ src/main/webapp/plugins/flow.js
@@ -22,6 +22,62 @@ Draw.loadPlugin(function(ui)
 		}
 	};
 
+	// var cellAdded = ui.editor.graph.model.prototype.cellAdded;
+	//
+	// ui.editor.graph.model.prototype.cellAdded = function()
+	// {
+	// 	var exp = cellAdded.apply(this, arguments);
+	// 	return exp;
+	// }
+
+
+
+
+	// const updateCellStyles = ui.editor.graph.updateCellStyles;
+	//
+	// ui.editor.graph.updateCellStyles = function(style,cells) {
+	// 	updateCellStyles.call(this,style, cell);
+	//
+	// 	// 检查自定义属性或进行其他修改
+	// 	if (cell.getAttribute('abc') === '1') {
+	// 		newCell.setStyle('strokeColor=#ff0000;strokeWidth=2;');
+	// 	}
+	//
+	// };
+
+	// mxEvent.UPDATE
+	function updateLine(cells){
+		for (var i in cells) {
+			// ui.editor.graph.model.getCells();
+			if (ui.editor.graph.model.isEdge(cells[i])) {
+				var state = ui.editor.graph.view.getState(cells[i]);
+
+				if (state && state.shape != null) {
+					var paths = state.shape.node.getElementsByTagName('path');
+					var style = ui.editor.graph.getModel().getStyle(cells[i]);
+					if (paths.length > 1) {
+						if(style&& style.indexOf("mxEdgeFlow")>=0){
+							paths[1].setAttribute('stroke-dasharray', '8');
+							paths[1].setAttribute('class', 'mxEdgeFlow');
+						}
+
+
+					}
+				}
+			}
+		}
+	}
+
+	ui.editor.graph.getView().addListener( mxEvent.UPDATE, function(sender, evt) {
+		updateLine(ui.editor.graph.model.getCells());
+	});
+	ui.editor.graph.getModel().addListener( mxEvent.UPDATE, function(sender, evt) {
+		var cells = sender.cells;
+		updateLine(cells);
+	});
+
+
+
 	//
 	// Main function
 	//
@@ -36,13 +92,13 @@ Draw.loadPlugin(function(ui)
 				if (state.shape != null)
 				{
 					var paths = state.shape.node.getElementsByTagName('path');
-					
+					var style = ui.editor.graph.getModel().getStyle(cells[i]);
 					if (paths.length > 1)
 					{
 						if (paths[1].getAttribute('class') == 'mxEdgeFlow')
 						{
 							paths[1].removeAttribute('class');
-			
+							// style = mxUtils.removeStylename(style, stylenames[j]);
 							if (mxUtils.getValue(state.style, mxConstants.STYLE_DASHED, '0') != '1')
 							{
 								paths[1].removeAttribute('stroke-dasharray');
@@ -50,13 +106,15 @@ Draw.loadPlugin(function(ui)
 						}
 						else
 						{
+
 							paths[1].setAttribute('class', 'mxEdgeFlow');
-			
+							style = mxUtils.setStyle(style, "mxEdgeFlow", "1");
 							if (mxUtils.getValue(state.style, mxConstants.STYLE_DASHED, '0') != '1')
 							{
 								paths[1].setAttribute('stroke-dasharray', '8');
 							}
 						}
+						ui.editor.graph.getModel().setStyle(cells[i], style);
 					}
 				}
 			}
@@ -64,7 +122,7 @@ Draw.loadPlugin(function(ui)
 	};
 	
 	// Adds action
-	ui.actions.addAction('toggleFlow', function()
+	ui.actions.addAction('动态线', function()
 	{
 		var cell = ui.editor.graph.getSelectionCell();
 		
@@ -88,6 +146,7 @@ Draw.loadPlugin(function(ui)
 	
 	try
 	{
+		debugger;
 		var style = document.createElement('style')
 		style.type = 'text/css';
 		style.innerHTML = ['.mxEdgeFlow {',
